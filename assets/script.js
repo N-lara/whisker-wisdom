1523486af84f6d872e190e7f6a809186ff068f6e
N-lara <n_lara01@yahoo.com>
2023-09-30 22:37:59 -0500

    reset due to api issues changing topic to cat facts

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var url='https://cdn2.thecatapi.com/images/FCNqMC83P.jpg'
+
+fetch(url)
+    .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
