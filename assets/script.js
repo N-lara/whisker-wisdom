75f201846c2c99ac1cd29808c630f16a393796d6
Tatum Sterling <tatum.sterling29@gmail.com>
2023-10-02 20:23:21 -0500

    added html

--- assets/script.js
+++ assets/script.js
@@ -13,3 +13,33 @@ fetch(url)
       .catch(function (error) {
         alert('Unable to connect to GitHub');
       });
+
+
+      $(document).ready(function () {
+        //dummy variables would need the right id for container
+        var catFacts = $("#random-fact");
+    //-------------------------------------
+        var requestUrl = "https://meowfacts.herokuapp.com/";
+        //Needs ID for button below
+        $("#next").on("click", function () {
+    //-------------------------------------------------------
+            retrieveCatFacts();
+        });
+
+        function retrieveCatFacts() {
+            fetch(requestUrl, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: "same-origin",
+                redirect: "follow"
+            }).then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log(data);
+                var catFactListed = data.data[0];
+                catFacts.text(catFactListed);
+            });
+        }
+    });
