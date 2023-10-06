1cb4536691ac36fb717a703fa4f80a82d8a1992e
Arthur Bridges <arthurbridges08@yahoo.com>
2023-10-06 18:52:31 -0500

    deletion of excess image and commented HTML

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 // calls both api's and creates array for photos and facts
-var viewingFavorites = 0
-var changeCheck = 0
+var viewingFavorites = 0;
+var changeCheck = 0;
 var catFacts = [];
 var catPhotos = [];
 var randomObject = [];
 var myFavorites = JSON.parse(localStorage.getItem("myFavorites")) || [];
 var currentArray = [];
-var ready = 0
+var ready = 0;
 
 function makeObject(){
   ready++
   if(ready === 2){
     for(var i = 0; i < catPhotos.length; i++){
       console.log(i);
-      random= {
+      random = {
         photo: catPhotos[i],
         fact: catFacts[i]
       }
@@ -41,9 +41,8 @@ fetch(catFactsUrl, {
   for (i = 0; i < data.data.length; i++) {
     // console.log(data.data);
     catFacts.unshift(data.data[i]);
-    // catF.text(catFacts);
   }
-  makeObject()
+  makeObject();
 });
 
 var catPhotosUrl = 'https://api.thecatapi.com/v1/images/search?limit=10'//50&api_key=live_4MxtfIDUxCcvH5y3DoACnMZw1ijvUw7eiz48xvUqelwA97DSk7Cpv1JkE4H64q84')
@@ -54,9 +53,8 @@ fetch(catPhotosUrl)
       response.json().then(function (data) {
         for (var i = 0; i < data.length; i++) {
           catPhotos.unshift(data[i].url);
-          // console.log(catPhotos);
         }
-        makeObject()
+        makeObject();
       });
     }
   });
@@ -80,7 +78,7 @@ function display(){
   console.log(currentArray[index].photo);
   console.log(currentArray[index].fact);
   image.attr('src', currentArray[index].photo);
-    catF.text(currentArray[index].fact);
+  catF.text(currentArray[index].fact);
 }
 
 function changeChecker(){
@@ -96,7 +94,7 @@ function changeChecker(){
 function next() {
   changeChecker();
   console.log('index'+index);
-  console.log(currentArray)
+  console.log(currentArray);
   console.log('array index'+currentArray[index]);
   if (index >= currentArray.length-1) {
     index = 0;
@@ -138,18 +136,17 @@ function addOrRemoveFav() {
     fact: currentFact
   };
 
-// 
-  //Modal
+ //Modal handles
+// $(".modal-background")
   var modal = $(".modal");
-  var modalBg = $(".modal-background");
   var modalContent = $("#modalContent");
+  //
   var isFavorite = false;
   for (var i = 0; i < myFavorites.length; i++) {
     if (myFavorites[i].photo === currentPhoto && myFavorites[i].fact === currentFact) {
       // If it's already in myFavorites, remove it
       myFavorites.splice(i, 1);
-      //call alerts function here in place of alert for modal
-      //alert("removed from favorites");
+      // ALERT MODAL
       modal.addClass("is-active");
       modalContent.text("Removed from favorites 💔");
       $(".modal-background").on("click", function () {
@@ -159,18 +156,16 @@ function addOrRemoveFav() {
       break;
     }
   }
-  //has-background-primary
-  // <h3 class="title mb-6" id = "modalContent"></h3>
+ 
   // If it's not in myFavorites, add it
   if (!isFavorite) {
     myFavorites.push(favoritePair);
-      //call alerts function here in place of alert for modal
+       // ALERT MODAL
       modal.addClass("is-active");
       modalContent.text("Added to favorites! 💕");
       $(".modal-background").on("click", function () {
         modal.removeClass("is-active");
       });
-    //  alert("added to favorites!")
   }
 
   // Store myFavorites in localStorage
@@ -180,8 +175,6 @@ function addOrRemoveFav() {
 // ...
 
 var viewFav = $("#view-favorites");
-
-
 
 
 $(viewFav).on("click", function () {
@@ -197,9 +190,9 @@ $(viewFav).on("click", function () {
 
   if (currentArray === myFavorites) {
     viewFav.text("back to random");
-    currentArray = randomObject
+    currentArray = randomObject;
   } else if (currentArray === randomObject) {
     viewFav.text("View Favorites")
-    currentArray = myFavorites
+    currentArray = myFavorites;
   }
 })
